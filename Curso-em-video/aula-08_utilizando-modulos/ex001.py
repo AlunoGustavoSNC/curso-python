7c010bb8b0af897db4022a909662ea0c9a5ffadc
Gustavo Nascimento <123gustaf321@gmail.com>
2023-09-11 23:50:44 -0300

    add aula 8

---
+++ Curso-em-video/aula-08_utilizando-modulos/ex001.py
@@ -0,0 +1,7 @@
+import math
+
+n = int(input('Digite um número: '))
+
+raiz = math.sqrt(n)
+
+print('A raiz de {} é {:.2f}' .format(n, raiz))
---
+++ Curso-em-video/aula-08_utilizando-modulos/ex002.py
@@ -0,0 +1,5 @@
+from math import sqrt
+
+r = sqrt(81)
+
+print(r)
